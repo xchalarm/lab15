c24e43d89ee0b2a3dd8a48dcdd7b7db15f77d4bf
xchalarm <tharana4671@gmail.com>
2023-01-17 15:21:32 +0700

    lab15_2.cppkub

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,29 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void inputMatrix(double A[][N]){
+  for(int o =0 ;o <N;o++){
+      cout << "Row " << o+1 << ": ";
+      for(int k =0 ;k <N;k++){cin >> A[o][k];}
+    }
+}
+void showMatrix(const bool B[][N]){
+    for(int o=0 ;o <N;o++){
+       for(int k =0 ;k <N;k++){
+         cout << B[o][k] << " ";}
+       cout <<'\n';
+    }
+}
+void findLocalMax(const double A[][N], bool B[][N]){
+    for(int ka = 0; ka < N ; ka++){
+		for(int a = 0; a < N ; a++){
+			if ((A[a][ka] >= A[a+1][ka]) && (A[a][ka] >= A[a-1][ka]) && (A[a][ka] >= A[a][ka-1]) && (A[a][ka] >= A[a][ka+1])){
+				if ((a != 0) && (a != N-1) && (ka != 0) && (ka != N-1)){
+				    B[a][ka] = 1;
+			    }
+				else B[a][ka] = 0;
+			}
+			else B[a][ka] = 0;
+		}
+	}
+}
